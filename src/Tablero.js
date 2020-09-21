0662aa9fd80b4117b1036298a2e829d1073063ad
Alex <alex2005.uy@gmail.com>
2020-09-21 16:24:10 -0300

    Comentarios a la solucion

--- src/Tablero.js
+++ src/Tablero.js
@@ -7,7 +7,7 @@ import { celdaReducer } from './helpers/CeldaReducer';
 
 export const Tablero = () => {
 
-    /* voy a dibujar el tablero, empiezo con una matriz multidimensional */
+    /* Este tablero inicial contiene los datos de inicio, en teoria podriamos tener un metodo que genera aleatoriamente un tablero */
     const tableroInicial = [
         [0, 0, 0, 4, 0, 7, 0, 0, 0],
         [9, 0, 1, 6, 8, 3, 0, 0, 0],
@@ -19,10 +19,10 @@ export const Tablero = () => {
         [3, 4, 7, 5, 9, 8, 1, 2, 6],
         [0, 5, 6, 1, 0, 0, 0, 0, 0],
     ];
-    let sectorACtivo = [
 
-
-    ];
+    /* Esta variable es una referencia al sector que actualmente esta en foco, un sector activo es una matriz de 3x3 que es un subconjunto
+    del tablero principal*/
+    let sectorACtivo = [];
 
 
     let grillaRef = useRef();
@@ -33,16 +33,23 @@ export const Tablero = () => {
 
     });
 
+
+    /* Este metodo tiene por objetivo detectar que celda se selecciono y poder operar con esto
+    La idea es que se llame a cambiar el sector, en este momento solo hay un metodo de prueba
+    por que no consegui hacer andar la funcion referenciada en el hijo (celda)
+    */
     const handleClick = (evento) => {
 
-        grillaRef.current.grillaRef.n.cambiarValor([0, 1], 5);
+       // grillaRef.current.grillaRef.n.cambiarValor([0, 1], 5);
 
     }
 
 
-
+    /* ConvertirGrilla es un helper que recibe una matriz de 9x9 y retorna un conjunto de celdas */
     const juego = converirGrilla(tableroInicial);
 
+    /* Utilizo un reducer para poder disparar cambios en el conjunto de celdas, la idea es poder cambiar cosas como el atributo
+    selected, el valor, etc */
     const [conjuntoCeldas, dispatch] = useReducer(celdaReducer, juego);
 
 
@@ -56,7 +63,10 @@ export const Tablero = () => {
         });
     }
 
-
+    /*  Metodo de prueba, la idea era probar la comunicacion, recibe la posicion que esa matriz de 2 numeros del estilo [0,1]
+        y un valor, hice este metodo de pruebas debido a que no estaba logrando esa comunicacion bidireccional entre los componentes
+        y de esa manera probar donde estaba el error
+    */
     const test = (posicion, valor) => {
         console.log('voy a cambiar el valor de', posicion);
         const fila = posicion[1];
@@ -78,7 +88,8 @@ export const Tablero = () => {
 
 
 
-
+    /* Este metodo iria ser el que de acuerdo a la celda que se seleccione en la grilla no editable (la matriz principal) se genera
+    una una matriz de 3x3 que es la que termina mostrando los datos a editar en la interfaz del usuario */
     const activarSector = (numero) => {
         sectorACtivo = [];
 
@@ -118,8 +129,11 @@ export const Tablero = () => {
                     <table className="table table-bordered col-12">
                         <tbody>
                             <tr >
-
+                               
                                 {
+                                     /* Esto deberia estar en otro componente <Grilla /> pero me daba problemas de comunicacion
+                                        por eso termine iterando el conjunto de celdas  y construyendo cada td
+                                    */
                                     conjuntoCeldas.map(celda => {
 
                                         if (celda.columna === 0) {
@@ -224,7 +238,8 @@ export const Tablero = () => {
                 </div>
                 <div className="col-3">
 
-                    {
+                    { /* Si el sector activo no es null significa que el usuario hizo clic en alguna celda y se activa el mismo, entonces
+                    se muestra el formulario de cambio que se carga con los datos de dicha variable */
                         (sectorACtivo) &&
                         <div>
                             <form>
@@ -246,7 +261,7 @@ export const Tablero = () => {
                                 onClick={() => { test([0, 1], 5) }}> prueba cambiarvalor</button>
                                  <button
                                 className="btn btn-blovk btn-primary"
-                                 onClick={()=> activarSector(2)}> prueba cambiarvalor</button>
+                                 onClick={()=> activarSector(2)}> prueba cambiarSector</button>
                         </div>
 
 
